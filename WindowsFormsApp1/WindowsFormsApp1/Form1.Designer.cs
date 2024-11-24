98a97f9136ea5c082fa09f1d38eac9b07d71ac78
derek569-hub <anthony.ramirez964@std.uni.edu.ni>
2024-11-24 17:37:37 -0600

    Tercer Repositorio

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.btnGuardarXml = new System.Windows.Forms.Button();
+            this.btnLeerXml = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(217, 138);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(137, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(189, 164);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(165, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(189, 190);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(165, 20);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProductos.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(285, 231);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.Size = new System.Drawing.Size(240, 150);
+            this.dgvProductos.TabIndex = 3;
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Location = new System.Drawing.Point(446, 135);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(109, 23);
+            this.btnAgregarProducto.TabIndex = 4;
+            this.btnAgregarProducto.Text = "Agregar Producto";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            this.btnAgregarProducto.Click += new System.EventHandler(this.btnAgregarProducto_Click);
+            // 
+            // btnGuardarXml
+            // 
+            this.btnGuardarXml.Location = new System.Drawing.Point(466, 164);
+            this.btnGuardarXml.Name = "btnGuardarXml";
+            this.btnGuardarXml.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardarXml.TabIndex = 5;
+            this.btnGuardarXml.Text = "Guardar";
+            this.btnGuardarXml.UseVisualStyleBackColor = true;
+            this.btnGuardarXml.Click += new System.EventHandler(this.btnGuardarXml_Click);
+            // 
+            // btnLeerXml
+            // 
+            this.btnLeerXml.Location = new System.Drawing.Point(466, 193);
+            this.btnLeerXml.Name = "btnLeerXml";
+            this.btnLeerXml.Size = new System.Drawing.Size(75, 23);
+            this.btnLeerXml.TabIndex = 6;
+            this.btnLeerXml.Text = "Leer";
+            this.btnLeerXml.UseVisualStyleBackColor = true;
+            this.btnLeerXml.Click += new System.EventHandler(this.btnLeerXml_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(558, 175);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir ";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(245, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestiones de Poductos  de CocaCola ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLeerXml);
+            this.Controls.Add(this.btnGuardarXml);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.Button btnGuardarXml;
+        private System.Windows.Forms.Button btnLeerXml;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<Producto> productos = new List<Producto>();
+        private const string RutaXml = "productos.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarProducto_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtId.Text, out int id) &&
+         double.TryParse(txtPrecio.Text, out double precio) &&
+         !string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                productos.Add(new Producto { Id = id, Nombre = txtNombre.Text, Precio = precio });
+                CargarProductosEnGrid();
+                LimpiarCampos();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese datos válidos.");
+            }
+        }
+
+        private void CargarProductosEnGrid()
+        {
+            dgvProductos.DataSource = null;
+            dgvProductos.DataSource = productos;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtId.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void btnGuardarXml_Click(object sender, EventArgs e)
+        {
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Producto>));
+            using (StreamWriter writer = new StreamWriter(RutaXml))
+            {
+                serializer.Serialize(writer, productos);
+            }
+            MessageBox.Show("Datos guardados en XML.");
+        }
+
+        private void btnLeerXml_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(RutaXml))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Producto>));
+                using (StreamReader reader = new StreamReader(RutaXml))
+                {
+                    productos = (List<Producto>)serializer.Deserialize(reader);
+                }
+                CargarProductosEnGrid();
+                MessageBox.Show("Datos cargados desde XML.");
+            }
+            else
+            {
+                MessageBox.Show("El archivo XML no existe.");
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
